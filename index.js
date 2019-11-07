6bb2a0f5fc5a11b7a85965ef1bd1ac206d1aaa09
matiasjj <matiasjj@metropolia.fi>
2019-11-07 13:23:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from my Node server');
+});
+
+app.get('/demo', (req, res) => {
+    console.log('request', req);
+    res.send('demo');
+});
+
+app.listen(3000, () => {
+    console.log('server app start?');
+});
