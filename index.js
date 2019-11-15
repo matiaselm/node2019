94847c8bf180ff7ddb690c5361d7f4cbe5f4990f
matiasjj <matiasjj@metropolia.fi>
2019-11-15 14:03:02 +0200

    database connection

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/animal', (req, res) => {
+    connection.query(
+        'SELECT * FROM animal',
+        function(err, results, fields){
+            console.log(results); // results contains rows r
+            console.log(fields); // fields contains extra m
+            res.json(results);
+        }
+    )
+});
 
 app.get('/', (req, res) => {
     res.send('Hello from my Node server');
@@ -18,3 +40,4 @@ app.get('/demo', (req, res) => {
 app.listen(3000, () => {
     console.log('server app start?');
 });
+
